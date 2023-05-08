008162d20ee41ef2660d3db8fce35e9d007c5e2f
Luis Vargas <76674314+JhayCodee@users.noreply.github.com>
2023-05-08 09:22:33 -0600

    implement - extends

--- 10_abstract_classes.dart
+++ 10_abstract_classes.dart
@@ -1,15 +1,27 @@
 void main() {
   
   final windPlant = WindPlant(initialEnergy: 100);
-  print(windPlant);
+  final nuclearPlant = NuclearPlant(energyLeft: 50);
+  
+  print('wind: ${chargePhone(windPlant)}');
+  print('nuclear: ${chargePhone(nuclearPlant)}');
 }
+
+
+double chargePhone( EnergyPlant plant ) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy');
+  }
+  
+  return  plant.energyLeft - 10;
+} 
 
 enum PlantType { nuclear, wind, water }
 
 abstract class EnergyPlant {
 
   double energyLeft;
-  PlantType type; // nuclear, wind, water
+  final PlantType type; // nuclear, wind, water
   
   EnergyPlant({
     required this.energyLeft,
@@ -27,5 +39,23 @@ class WindPlant extends EnergyPlant {
   @override
   void consumeEnergy (double amount) {
     energyLeft -= amount;
+  }
+}
+
+
+class NuclearPlant implements EnergyPlant {
+  
+  @override
+  double energyLeft;
+  
+  @override
+  final PlantType type = PlantType.nuclear;
+  
+  //ctor
+  NuclearPlant({ required this.energyLeft });
+  
+  @override
+  void consumeEnergy (double amount) {
+    energyLeft -= (amount * 0.5);
   }
 }
